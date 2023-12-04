488d75708693799953a4aa50fd9164b37b4a79e1
Noah Yorgey <noah.yorgrey@gmail.com>
2023-12-04 17:54:27 -0600

    Put Version Check link in place

--- update.ps1
+++ update.ps1
@@ -1,4 +1,9 @@
-Invoke-WebRequest "https://" -OutFile ./versionCheckTemp
-if (Get-Content .\version == ) {
-    <# Action to perform if the condition is true #>
+Invoke-WebRequest "https://raw.githubusercontent.com/noahyor/PSTools/main/version" -OutFile .\versionCheckTemp
+if (!((Get-Content .\version -Raw) -eq (Get-Content .\versionCheckTemp -Raw))) {
+    Write-Output "Download available for PSTools, would you like to download it?"
+} else {
+    
 }
+Remove-Item .\versionCheckTemp
+Write-Output "Version is the same: TEST"
+Pause
